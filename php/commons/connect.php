101a571c2f4cecbec43df4840fc7ed57fd7177da
danielegiulianini <daniele.giulianini3@studio.unibo.it>
2019-06-29 16:36:07 +0200

    prepare first prototype for admin menu page

--- php/commons/connect.php
+++ php/commons/connect.php
@@ -1,5 +1,5 @@
 <?php
-$servername="localhost"; //questo lo potrei rifattorizzare in un file esterno volendo
+$servername="localhost";
 $username="root";
 $password="";
 $db="unifood";
---
+++ php/startPage.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <?php include($_SERVER['DOCUMENT_ROOT']."/progettoweb/php/administrator/layout/references.php"); ?>
+    <!--<link href="/progettoweb/css/administrator/common.css" rel="stylesheet">
+    <script src="/progettoweb/js/administrator/clients.js"></script>-->
+<style>
+
+</style>
+
+
+</head>
+<body>
+  <main role="main">
+    <section class="jumbotron text-center">
+      <div class="container">
+        <h1 class="jumbotron-heading">Fishes Diagnosis</h1>
+        <p class="lead text-muted">Admin account</p>
+      </div>
+
+    <div class="album py-5 bg-light">
+      <div class="container ">
+        <div class="row d-flex align-items-center">
+          <div class="col-md-3 mx-auto">
+            <div class="card box-shadow text-center pb-0">
+                <a class="btn btn-secondary m-2">test</a>
+                <a class="btn btn-secondary m-2">test</a>
+                <a class="btn btn-secondary m-2">test</a>
+            </div>
+          </div>
+
+            <div class="col-md-3 mx-auto">
+              <div class="card box-shadow text-center">
+                  <a class="btn btn-secondary m-2">test</a>
+                  <a class="btn btn-secondary m-2">test</a>
+                  <a class="btn btn-secondary m-2">test</a>
+              </div>
+          </div>
+          <div class="col-md-3 mx-auto">
+            <div class="card box-shadow text-center">
+                <a class="btn btn-secondary m-2">test</a>
+                <a class="btn btn-secondary m-2">test</a>
+                <a class="btn btn-secondary m-2">test</a>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+  </main>
+
+  <footer class="text-muted">
+    <div class="container">
+      <p class="text-center">footer</p>
+    </div>
+  </footer>
+<body>
