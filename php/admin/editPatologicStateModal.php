f96a308e6a6e93952ad9cfbeb7f474ae50ea5f26
danielegiulianini <daniele.giulianini3@studio.unibo.it>
2019-07-04 15:03:16 +0200

    add editPatologic state modal  as a php file (not completed yet)

---
+++ php/admin/editPatologicStateModal.php
@@ -0,0 +1,35 @@
+<!--Modal for inserting new patologic state-->
+<script src="/fishesdiagnosis/js/admin/addPatologicStateModal.js"></script>
+
+<div id="add-pat-st-modal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Nuovo stato patologico</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      <div class="modal-body">
+        <form id="add-pat-st-form">
+            <fieldset class="form-group">
+              <label for="nomeStato">Nome</label>
+              <input type="text" class="form-control nome" name="nomeStato" id="nomeStato" required>
+
+
+                <label for="tipoStato">Tipologia</label>
+                <select class="form-control" name="tipoStato" id="tipoStato" style="display: inline-block"><!--i could have used 2 radios-->
+                  <option value="critico" selected>critico</option>
+                  <option value="patologico">patologico</option>
+                </select>
+
+            </fieldset>
+          </form>
+          </div><!--modal body--->
+      <div class="modal-footer">
+        <button type="button" id="confirm-add-pat-st-button" class="btn btn-secondary">Conferma inserimento stato patologico</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Annulla</button>
+      </div>
+    </div><!--modal-content-->
+</div><!--modal dialog-->
+</div><!--modal-->
