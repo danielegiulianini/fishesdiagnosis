a825189211a499556eb5e61af6e8c78348cdb68c
danielegiulianini <daniele.giulianini3@studio.unibo.it>
2019-07-09 21:46:10 +0200

    add script to port data

--- php/commons/pages/conversion.php
+++ php/commons/pages/conversion.php
@@ -37,18 +37,38 @@ $conn2->set_charset("utf8"); //fondamentale per encoding caratteri
 
 
 
-
-
+/*
+  presentazioni:
   $sql="select * from presentazioni";
   $result = $conn2->query($sql);
   while($row = $result->fetch_assoc()) {
     if ($row["specie"]!="cefalo"){
       //$sql = "insert into presentazioni(idStatoPat, specie,idSegno, gradoFrequenza) values (".$row["idStatoPat"].", ".$row["specie"].", ".$row["idSegno"].", ".$row["grado_frequenza"].")";
-    /*  $sql= "insert into presentazioni(idStatoPat, specie,idSegno, gradoFrequenza) values (".$row["idStatoPat"].", '".$row["specie"]."', ".$row["idSegno"].", ".$row["grado_frequenza"].") WHERE NOT EXISTS (
-    SELECT * FROM presentazioni WHERE idStatoPat = " .$row["idStatoPat"]." and specie='".$row["specie"]."' and idSegno =".$row["idSegno"].")";
-      */
+
       $sql = "insert into presentazioni(idStatoPat, specie,idSegno, gradoFrequenza) values (".$row["idStatoPat"].", '".$row["specie"]."', ".$row["idSegno"].", ".$row["grado_frequenza"].")";
       $conn -> query($sql);
     }
-  }
+  }*/
+
+/*
+//segni presnti(scheda 2(115) , 1(114) c'è già)
+$sql="select * from segnipresenti where idScheda=115";
+$result = $conn2->query($sql);
+while($row = $result->fetch_assoc()) {
+
+    //$sql = "insert into presentazioni(idStatoPat, specie,idSegno, gradoFrequenza) values (".$row["idStatoPat"].", ".$row["specie"].", ".$row["idSegno"].", ".$row["grado_frequenza"].")";
+    $sql = "insert into segnipresenti(idSegno, idScheda, percentuale) values (".$row["idSegno"].", 9, ".$row["percentuale"].")";
+    $conn -> query($sql);
+}
+
+//segni assenti(scheda 2(115) , 1(114) c'è già)
+$sql="select * from segniassenti where idScheda=115";
+$result = $conn2->query($sql);
+while($row = $result->fetch_assoc()) {
+     $sql = "insert into segniassenti(idSegno, idScheda) values (".$row["idSegno"].", 9)";
+    $conn -> query($sql);
+}
+*/
+
+
 ?>
