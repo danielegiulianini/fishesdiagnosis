241e0d00d2e0a7de0eda55c40f063024c763b478
danielegiulianini <daniele.giulianini3@studio.unibo.it>
2019-07-09 22:01:27 +0200

    clean patStateInfoGetter

--- php/admin/scripts/patologicStateInfoGetter.php
+++ php/admin/scripts/patologicStateInfoGetter.php
@@ -6,8 +6,8 @@ include($_SERVER['DOCUMENT_ROOT']."/fishesdiagnosis/php/commons/scripts/checkLog
 include($_SERVER['DOCUMENT_ROOT']."/fishesdiagnosis/php/commons/connect.php");
 
 
-/*The task of this file is fetching datatbles od viewReportPage (general info table
- is fetched directly in viewReportPage because is styling is particular and because
+/*The task of this file is fetching datatbles od viewPatologicStatePage (general info table
+ is fetched directly in viewPatologicStatePage because its styling is particular and because
  of that it is not a datatable). datatables ask for info by contacting this
  url trough thier js file. DataTables could also be fetched filling tables in the dom.
 In future I could put this code in the DOM directly.*/
@@ -44,31 +44,27 @@ $idStatoPat = $_POST["idStatoPat"];
 
 switch($subject){
   case "present_signs":
-//echo "select * from segnipresenti inner join segni on segni.idSegno=segnipresenti.idSegno where segnipresenti.idScheda = ".$idScheda;
+
     fillOutputWithQueryResults("select * from statipatologici inner join presentazioni on statipatologici.idStatoPat=presentazioni.idStatoPat where statipatologici.idStatoPat = ".$idStatoPat, $conn, $output);//yes need of join
 
     break;
   case "measurements":
 
-    fillOutputWithQueryResults("select * from misurazioni where idScheda = ".$idScheda, $conn, $output); //no need of join
+    //fillOutputWithQueryResults("select * from misurazioni where idScheda = ".$idScheda, $conn, $output); //no need of join
 
     break;
 
   case "events":
 
-      fillOutputWithQueryResults("select * from eventi where idScheda = ".$idScheda, $conn, $output); //no need of join
+    //fillOutputWithQueryResults("select * from eventi where idScheda = ".$idScheda, $conn, $output); //no need of join
 
   break;
 
   case "conclusions":
 
-    fillOutputWithQueryResults("select * from conclusioni where idScheda = ".$idScheda, $conn, $output); //no need of join
+    //fillOutputWithQueryResults("select * from conclusioni where idScheda = ".$idScheda, $conn, $output); //no need of join
 
     break;
-}
-
-if (!is_null ($stmt)){ //if fields required are not set this prevents from executing
-  $stmt->execute();
 }
 
 $conn->close();
